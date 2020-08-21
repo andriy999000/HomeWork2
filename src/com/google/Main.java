12e1a14fbf5f6c3a1bc1cdca814a03e17768e335
andriy999000 <vladRydenko77Agmail.com>
2020-08-21 13:37:02 +0300

    Initial commit

---
+++ src/com/google/Main.java
@@ -0,0 +1,31 @@
+package com.google;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        byte b = 10;
+        short s = 100;
+        int i = 1000;
+        long l = 10000L;
+
+        float f = 100.5f;
+        double _d = 1000.5;
+
+        char c = '\u2713';
+
+        boolean b2 = true;
+
+
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(_d);
+        System.out.println(c);
+        System.out.println(b2);
+
+
+    }
+}
